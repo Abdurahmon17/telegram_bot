95a6071fe7987eef49632661ada40e120d5ddb4b
Abdumavlonov Abdurahmon <abdumavlonovabdurahmon75@gmail.com>
2025-11-06 16:30:41 +0500

    fix error

--- main.py
+++ main.py
@@ -1,22 +1,20 @@
 from aiogram import Bot, Dispatcher, types, F
 from aiogram.filters import Command
 import asyncio
+from keep_alive import keep_alive
 
-TOKEN = "BotToken"
-CHANNEL_ID = "Kana username yoki kanal_id"
+TOKEN = "8568305074:AAGldekwMYk_Y_-04bZC0hlO5bR9Nn5fRAc"
+CHANNEL_ID = "https://t.me/+LuA1Cm2Jjh83Yjc6"
+
 
 bot = Bot(TOKEN)
 dp = Dispatcher()
-
-# Xotirada saqlash
 kodlar = {}  # kod: file_id
 
-# /start
 @dp.message(Command("start"))
 async def start(m: types.Message):
     await m.answer("🎬 Kino kodini yuboring. Videolar kanal orqali olinadi.")
 
-# Kanalga yangi video post kelganda
 @dp.channel_post(F.video)
 async def new_video(post: types.Message):
     if post.caption and "kod:" in post.caption.lower():
@@ -25,9 +23,8 @@ async def new_video(post: types.Message):
             kodlar[kod] = post.video.file_id
             print(f"Kod {kod} saqlandi!")
         except:
-            print("Caption formatida xato bor:", post.caption)
+            print("Caption formatida xato:", post.caption)
 
-# Foydalanuvchi kod yuborganida
 @dp.message(F.text.regexp(r"^\d+$"))
 async def user_kod(m: types.Message):
     kod = int(m.text)
@@ -37,8 +34,8 @@ async def user_kod(m: types.Message):
     else:
         await m.answer("❌ Kod topilmadi")
 
-# Run bot
 async def main():
+    keep_alive()  # bu serverni 24/7 ish holatda ushlab turadi
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
